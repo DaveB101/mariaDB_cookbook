c796d99f4c317638d9901fad51317e2ff0deeb5f
DaveB101 <33671753+DaveB101@users.noreply.github.com>
2017-12-04 21:45:22 +0000

    Update metadata.rb

--- mariaDB/metadata.rb
+++ mariaDB/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'Dave@example.com'
 license 'Apache 2.0'
 description 'Installs/Configures mariaDB'
 long_description 'Installs/Configures mariaDB'
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 depends          'apt'
