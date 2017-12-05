6cbac1e4d8cd8e1da9e0189239472c4df95b093d
DaveB101 <33671753+DaveB101@users.noreply.github.com>
2017-12-05 14:44:35 +0000

    Update metadata.rb

--- mariaDB/metadata.rb
+++ mariaDB/metadata.rb
@@ -5,4 +5,5 @@ license 'Apache 2.0'
 description 'Installs/Configures mariaDB'
 long_description 'Installs/Configures mariaDB'
 version '0.2.0'
+source_url 'https://github.com/DaveB101/mariaDB_cookbook'
 chef_version '>= 12.1' if respond_to?(:chef_version)
